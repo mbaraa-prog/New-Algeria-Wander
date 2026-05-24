f4227d1b61a75df9a7b8f7dbab94fcf64eb21216
Mohammed Baraa <m_chaalel@estin.dz>
2026-05-24 09:28:16 +0100

    Deploying latest Algeria Wander features1

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8000/api/';
+const baseURL = import.meta.env.VITE_API_URL ||'http://localhost:8000/api/';
 
 const axiosInstance = axios.create({
   baseURL,
