1c38eed0a3250390b4bcc95db1cbdf91bcbf31f9
Mohammed Baraa <m_chaalel@estin.dz>
2026-05-26 16:55:02 +0100

    fix wsgi crash

--- backend/project_name/wsgi.py
+++ backend/project_name/wsgi.py
@@ -1,6 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings")
-from project_name.utils.init_admin import create_admin_if_not_exists
-create_admin_if_not_exists()
 application = get_wsgi_application()
