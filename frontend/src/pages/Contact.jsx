5cf81655a9c5b86aca5bb48a0c1db6fc16df98d0
houssem eddine mansouri <h_mansouri@estin.dz>
2026-06-02 22:44:52 +0100

    image

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -27,33 +27,33 @@ const LocationIcon = () => (
 );
 
 const InstagramIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+  <svg xmlns="https://www.instagram.com/stat___.___/?__pwa=1" className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
     <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
   </svg>
 );
 
 const YouTubeIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+  <svg xmlns="https://youtu.be/DLMkUr_GIIc?si=Tua6bd-8Wllts7Qc" className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
     <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
   </svg>
 );
 
-const ArrowIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2.5} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+const MAILcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2-2v10a2 2 0 002 2z" />
   </svg>
 );
 
 const contactInfo = [
-  { Icon: EmailIcon, label: 'EMAIL', value: 'algeriawander@gmail.com' },
-  { Icon: PhoneIcon, label: 'PHONE', value: '+213 551 35 0989' },
+  { Icon: EmailIcon, label: 'EMAIL', value: 'h_mansouri@estin.dz' },
+  { Icon: PhoneIcon, label: 'PHONE', value: '+213 657442146' },
   { Icon: LocationIcon, label: 'OFFICE', value: 'ESTIN, Amizour, Béjaïa, Algérie' },
 ];
 
 const socialLinks = [
   { label: 'Website', href: '#', Icon: WebIcon },
-  { label: 'Instagram', href: 'https://instagram.com/algeriawander', Icon: InstagramIcon },
-  { label: 'YouTube', href: 'https://youtube.com/@algeriawander', Icon: YouTubeIcon },
+  { label: 'Instagram', href: 'https://www.instagram.com/stat___.___/?__pwa=1', Icon: InstagramIcon },
+  { label: 'YouTube', href: 'https://youtu.be/DLMkUr_GIIc?si=Tua6bd-8Wllts7Qc', Icon: YouTubeIcon },
 ];
 
 const Contact = () => {
